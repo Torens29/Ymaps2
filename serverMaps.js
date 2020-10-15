e7ee236910898f860fe200953c91368aa29aeb80
Torens29 <44534255+Torens29@users.noreply.github.com>
2020-10-15 14:56:57 +0300

    переделал глобальный объект с объектами)

--- serverMaps.js
+++ serverMaps.js
@@ -1,17 +1,17 @@
  let express =  require('express');
  cookieParser = require('cookie-parser');
-const axios = require('axios');
 const MongoClient = require("mongodb").MongoClient;
-// const { shouldSendSameSiteNone } = require('should-send-same-site-none');
 
 
 let db, dbClient, collection, arrOfcookies = [];
 let mongoClient = new MongoClient("mongodb://localhost:27017/", { useNewUrlParser: true ,useUnifiedTopology: true});
 
-let objects = {
-    type: 'FeatureCollection',
-    features: []     
-};
+let objOfObjs;
+
+// let objects = {
+//     type: 'FeatureCollection',
+//     features: []     
+// };
 
 mongoClient.connect(function(err, client){
 
@@ -29,7 +29,6 @@ mongoClient.connect(function(err, client){
 });
 
 let app =  express();
-// app.use(shouldSendSameSiteNone);
 app.use(express.static(__dirname + "/public"));
 app.use(cookieParser('secret key'));
 
@@ -50,24 +49,23 @@ app.get('/q', function (req, res) {
     requestBbox.forEach((element, index, arr) => {
         arr[index] = parseFloat(element);
     });
-    console.log(requestBbox);
-    sendFeatures(requestBbox).then(obj => res.jsonp(obj));
+    // console.log(requestBbox);
+
+    sendFeatures(requestBbox)
+            .then(obj =>{
+                res.jsonp(obj);
+            });
+           
 });
 
 async function sendFeatures(requestBbox){
-    // await  fPush(featuresDB, requestBbox);//.catch(err => {console.log(err)});
-    await find(requestBbox);
-    console.log('send:', objects);
-    return(objects);
+    // await find(requestBbox);
+    console.log('send:', await find(requestBbox));
+    return(await find(requestBbox));
 }
 
 function find(requestBbox){
     let coordinates = [
-        // [requestBbox[0],requestBbox[1]],
-        // [requestBbox[0],requestBbox[3]],
-        // [requestBbox[2], requestBbox[3]], 
-        // [requestBbox[2], requestBbox[1]], 
-        // [requestBbox[0],requestBbox[1]]
         [requestBbox[1],requestBbox[0]],
         [requestBbox[3],requestBbox[0]],
         [requestBbox[3], requestBbox[2]], 
@@ -75,7 +73,10 @@ function find(requestBbox){
         [requestBbox[1],requestBbox[0]]
     ];
 
-    // console.log('coordinates',coordinates);
+    // let objects = {
+    //     type: 'FeatureCollection',
+    //     features: []     
+    // };
 
     collection.find({
         coords:{
@@ -87,30 +88,37 @@ function find(requestBbox){
             }
         }    
     }).toArray(function(err, results){
-                // console.log("results", results);
 
-                results.forEach(el =>{
+        // console.log("results", results);
+        let objects = {
+            type: 'FeatureCollection',
+            features: []     
+        };
+        results.forEach(el =>{
 
-                    if(!arrOfcookies.includes(el.id.toString())){
-                        
-                        console.log('EL true', arrOfcookies, el.id.toString());
-
-                        objects.features.push({
-                            'type': 'Feature',
-                            'id': el.id,
-                            'geometry': {
-                                "type": "Point",
-                                "coordinates": [el.coords.coordinates[1], el.coords.coordinates[0]]
-                            },
-                            'properties':{
-                                "balloonContent": el.name + el.address,
-                                // "clusterCaption": "Метка "+featuresDB[i].id,
-                                "hintContent": el.address// featuresDB[i].name
-                            }
-                        });
+            if(!arrOfcookies.includes(el.id.toString())){
+                
+                // console.log('EL true', arrOfcookies, el.id.toString());
+                
+                objects.features.push({
+                    'type': 'Feature',
+                    'id': el.id,
+                    'geometry': {
+                        "type": "Point",
+                        "coordinates": [el.coords.coordinates[1], el.coords.coordinates[0]]
+                    },
+                    'properties':{
+                        "balloonContent": el.name + el.address,
+                        // "clusterCaption": "Метка "+featuresDB[i].id,
+                        "hintContent": el.address// featuresDB[i].name
                     }
                 });
+                
+            }
+        });
+        objOfObjs = objects;
     });
+    return objOfObjs;
 }
 
   
